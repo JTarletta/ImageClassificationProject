c256d684d1370f177ced1701a43db0a817d0293f
JTarletta <48681725+JTarletta@users.noreply.github.com>
2022-03-23 06:01:51 -0300

    Add files via upload

---
+++ Image_Classification/app.py
@@ -0,0 +1,33 @@
+from distutils.log import debug
+from flask import Flask, render_template, request, url_for, redirect
+from clasificador import predictor
+from werkzeug.utils import secure_filename
+#print(Flask)
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def principal():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def prediction():
+    imageFile = request.files['ImageFile'] 
+    image_path = "./images/" + imageFile.filename
+    imageFile.save(image_path)
+
+    classification = predictor(image_path=image_path)
+
+#    filename = secure_filename(imageFile.filename) #Para hacer display de la imagen
+
+    return render_template('index.html', prediction=classification) #, filename=filename)
+
+#@app.route('/display/<filename>')
+#def display_image(filename):
+#    print('display_image filename: ' + filename)
+#    return redirect(url_for('images', filename='' + filename), code=301)
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
---
+++ Image_Classification/clasificador.py
@@ -0,0 +1,54 @@
+from email.mime import image
+import torch
+from torchvision import models, transforms #Modelos, y Transformaciones para ajustal la imagen 
+from PIL import Image
+from io import BytesIO #Lectura de la Imagen
+
+#Cargamos el diccionario de Etiquetas de ImageNet
+with open("resources\imageNet_index.txt") as f:
+    idx2label = eval(f.read())
+#print(idx2label, idx2label.get(916))
+
+#Imagen a clasificar
+imagen = Image.open("images\prueba.jpeg")
+#imagen.show()
+
+def predictor(image_path=imagen):
+    # print(dir(models)) #Modelos de clasificación de imágenes
+
+    imagen = Image.open(image_path)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    #print(device) #Computo utilizado
+
+    #Seleccionamos el modelo a utilizar
+    alexnet = models.alexnet(pretrained=True)
+    #print(alexnet)
+
+
+    #Transformador para ajustar la imagen al número de neuronas de la red
+    preprocess = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225]
+            )])
+    #print(preprocess)
+
+    
+    img_t = preprocess(imagen)                # preprocesar imagen
+    batch_t = torch.unsqueeze(img_t, 0)    # convertir a tensor de 1-dimension
+    alexnet.eval()          # entrar en modo evaluación
+    out = alexnet(batch_t)  # pasar la imagen ya preprocesada y ajustada
+
+    _, indices = torch.sort(out, descending=True)
+    
+    #print(indices[0][:3]) #3 Predicciones de la red según los Indices de ImageNet
+    clave = indices[0][:1].item()
+
+    clasification = idx2label.get(clave)
+
+    return clasification
